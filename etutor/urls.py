d2c9425a0a6b75700e330b7bf46a9ac26856c6da
simatei <dr.robsimatei@gmail.com>
2019-08-24 15:23:52 +0300

    Update url configurations

--- etutor/urls.py
+++ etutor/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tutor.urls'))
+    path('', include('tutor.urls')),
+    path('', include('frontend.urls'))
 ]
