0770971ce9df3bdd7a5e9b244686d20fe8680a51
simatei <dr.robsimatei@gmail.com>
2019-08-24 13:14:53 +0300

    Create model for tutor profiles

--- tutor/models.py
+++ tutor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Tutor Profile
+class TutorProfile(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    specialization = models.CharField(max_length=30)
+    created_on = models.DateTimeField(auto_now_add=True)
+
