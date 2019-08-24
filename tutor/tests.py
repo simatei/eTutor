8626355a813bce7da3719fd3949fa23e95181254
simatei <dr.robsimatei@gmail.com>
2019-08-24 13:15:31 +0300

    Create Initial tests for Tutor Profile

--- tutor/tests.py
+++ tutor/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import TutorProfile
+from datetime import datetime
 
-# Create your tests here.
+# Tutor Profile Test
+class TestTutorProfile(TestCase):
+    def setUp(self):
+        self.Bob = TutorProfile(first_name='Bob', last_name='Axe', email='ba@etutor.com',
+                                specialization='trading 101', created_on=datetime.now() )
+
+    def test_instance(self): # Test if is instance of Tutor Profile
+        self.assertTrue(isinstance(self.Bob, TutorProfile))
+
+    def tearDown(self):
+        TutorProfile.objects.all().delete()
+
