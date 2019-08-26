3ad183315ff4198ded7eb85819e53acce84d2f73
simatei <dr.robsimatei@gmail.com>
2019-08-26 17:33:49 +0300

    Create new frontend for eTutor

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,179 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility
+} from "semantic-ui-react";
+
+const getWidth = () => {
+  const isSSR = typeof window === "undefined";
+  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
+};
+
+class DesktopContainer extends Component {
+  state = {};
+
+  hideFixedMenu = () => this.setState({ fixed: false });
+  showFixedMenu = () => this.setState({ fixed: true });
+
+  render() {
+    const { children } = this.props;
+    const { fixed } = this.state;
+
+    return (
+      <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+        />
+        {children}
+      </Responsive>
+    );
+  }
+}
+
+DesktopContainer.propTypes = {
+  children: PropTypes.node
+};
+
+class MobileContainer extends Component {
+  state = {};
+
+  handleSidebarHide = () => this.setState({ sidebarOpened: false });
+
+  handleToggle = () => this.setState({ sidebarOpened: true });
+
+  render() {
+    const { children } = this.props;
+    const { sidebarOpened } = this.state;
+
+    return (
+      <Responsive
+        as={Sidebar.Pushable}
+        getWidth={getWidth}
+        maxWidth={Responsive.onlyMobile.maxWidth}
+      >
+        {children}
+      </Responsive>
+    );
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node
+};
+
+const ResponsiveContainer = ({ children }) => (
+  <div>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </div>
+);
+
+ResponsiveContainer.propTypes = {
+  children: PropTypes.node
+};
+
+const HomepageLayout = () => (
+  <ResponsiveContainer>
+    <Segment style={{ padding: "8em 0em" }} vertical>
+      <Grid container stackable verticalAlign="middle">
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              eTutor, the modern way to learn
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              Easy effective learning anywhere.
+            </p>
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              Catchy passphrase
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+             Vindicate catchy passphrase
+            </p>
+          </Grid.Column>
+          <Grid.Column floated="right" width={6}>
+            <Image
+              bordered
+              rounded
+              size="large"
+              src="/images/wireframe/white-image.png"
+            />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column textAlign="center">
+            <Button size="huge">Courses</Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment style={{ padding: "0em" }} vertical>
+      <Grid celled="internally" columns="equal" stackable>
+        <Grid.Row textAlign="center">
+          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              To do
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              Featured 1
+            </p>
+          </Grid.Column>
+          <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
+            <Header as="h3" style={{ fontSize: "2em" }}>
+              Featured 2
+            </Header>
+            <p style={{ fontSize: "1.33em" }}>
+              <Image avatar src="" />
+              <b>Nan</b> To do
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment style={{ padding: "8em 0em" }} vertical>
+      <Container text>
+        <Header as="h3" style={{ fontSize: "2em" }}>
+          Grid to do
+        </Header>
+        <p style={{ fontSize: "1.33em" }}>
+          to do
+        </p>
+        <Button as="a" size="large">
+          Read More
+        </Button>
+        <Divider
+          as="h4"
+          className="header"
+          horizontal
+          style={{ margin: "3em 0em", textTransform: "uppercase" }}
+        >
+          <a href="#">Case Studies</a>
+        </Divider>
+        <Header as="h3" style={{ fontSize: "2em" }}>
+          To do
+        </Header>
+        <p style={{ fontSize: "1.33em" }}>
+         To do
+        </p>
+        <Button as="a" size="large">
+          To do
+        </Button>
+      </Container>
+    </Segment>
+  </ResponsiveContainer>
+);
+export default HomepageLayout;
