510016b4daf9e643fc23d32d15efe93a9cbf9539
simatei <dr.robsimatei@gmail.com>
2019-09-01 13:11:28 +0300

    Remove deleted frontend app from registered apps

--- etutor/settings.py
+++ etutor/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tutor',
-    'frontend',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
